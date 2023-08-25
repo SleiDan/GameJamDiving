9610b4a5d930ce2f4bb24d9665180d1c26ca5a0e
SleiDan <dan.shvedun@gmail.com>
2023-08-25 19:47:57 +0300

    Miving in X

--- My project/Assets/Scripts/MoveBackground.cs
+++ My project/Assets/Scripts/MoveBackground.cs
@@ -12,7 +12,7 @@ public class MoveBackground : MonoBehaviour
     void Start()
     {
         back_Tranform = GetComponent<Transform>();
-        back_Size = GetComponent<SpriteRenderer>().bounds.size.y;
+        back_Size = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -35,6 +35,6 @@ public class MoveBackground : MonoBehaviour
     {
         back_pos += speed * Time.deltaTime;
         back_pos = Mathf.Repeat(back_pos, back_Size);
-        back_Tranform.position = new Vector3(0, back_pos, 0);
+        back_Tranform.position = new Vector3(-back_pos, 0, 0);
     }
 }
